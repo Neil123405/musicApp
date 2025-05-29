d46758b7687d3647a104e470dcf5270d9a263516
Neil <156774573+Neil123405@users.noreply.github.com>
2025-05-29 09:45:23 +0800

    CONFIDENCE

--- src/app/services/download-state.service.ts
+++ src/app/services/download-state.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DownloadStateService {
+  // This line creates a private property called `downloadingSubject` and initializes it as a new `BehaviorSubject` from the RxJS library, with an initial value of `false`. A `BehaviorSubject` is a special type of observable that always holds the latest value and emits it to any new subscribers immediately upon subscription. By using `<boolean>`, it specifies that this subject will only emit boolean values. The initial value of `false` indicates that, by default, no download is in progress. Making this property `private` ensures that it can only be accessed or modified within the service, enforcing encapsulation and allowing controlled updates to the download state through dedicated methods. This pattern is commonly used in Angular and reactive JavaScript applications to manage and broadcast state changes across different parts of the app.
   private downloadingSubject = new BehaviorSubject<boolean>(false);
   // mostly for the app component to subscribe to this observable
   isDownloading$ = this.downloadingSubject.asObservable();
