9123bd42a30c1aae285d28c1921c2894a2ea97cc
Neil <156774573+Neil123405@users.noreply.github.com>
2025-06-03 00:15:14 +0800

    JAMENDO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,10 @@ export class AppComponent {
   // Property decorator that configures a view query. The change detector looks for the first element or the directive matching the selector in the view DOM. If the view DOM changes, and a new child matches the selector, the property is updated.
   // Angular has a change detector
   // IonMenu is a reference to the IonMenu component class.
-
+  // think of it this way as the view changes, @viewchild tells angular to look for the first IonMenu element in the view DOM and assign it to the menu property.
+  // The ! assures Typescript compiler that the menu property will be defined after the view is initialized, so it won't throw an error when accessing it
+  // if no ! is used then an error will be thrown saying that the menu property is possible undefined
+  // Without it, TypeScript will require you to handle the case where menu might be undefined. or just remove it and type this.menu in an if statement
   @ViewChild(IonMenu) menu!: IonMenu;
 
   isDownloading = false;
